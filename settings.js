6eda31a81c82d28e9dabdd975b89b4061cd1c683
Bohdan Suslovych <suslovichb@gmail.com>
2022-01-14 10:35:49 +0200

    Fix multitab users/repos writing issue

--- settings.js
+++ settings.js
@@ -1,42 +1,106 @@
 $( document ).ready(function() {
-    
-    let storageState = {
-        users: [],
-        repositories: [],
-        ght: '',
+
+    let users = new Set();
+    let repositories = new Set();
+
+    const setUsers = (newUsers) => {
+        users = new Set(newUsers);
     }
 
-    const updateStorageState = (newState) => {
-        storageState = {...storageState, ...newState};
+    const setRepositories = (newRepositories) => {
+        repositories = new Set(newRepositories);
     }
 
-    const loadStorage = () => {
-        users = chrome.storage.local.get('users');
-        repositories = chrome.storage.local.get('repositories');
-        ght = chrome.storage.local.get('ght');
-    } 
-    
-    const saveStorage = () => {
-        for (const [key, value] of Object.entries(storageState)) {
-            chrome.storage.local.set({key: JSON.stringify(value)});
-          }
-    } 
+    const loadLocalUsers = () => {
+        chrome.storage.local.get("users", (storage) => {
+            loadedUsers = JSON.parse(storage.users);
+            setUsers(loadedUsers);
+        })
+    }
 
-    $("#add-user-form").submit(function(event) {
+    const addUser = (user) => {
+        loadLocalUsers();
+        users.add(user);
+        chrome.storage.local.set(
+            {"users": JSON.stringify([...users])},
+            () => { 
+                chrome.storage.local.get("users", (storage) => {
+                    console.log(storage);
+                }) 
+             }
+        );
+    }
+
+    $("#add-user-form").submit(event => {
         event.preventDefault();
-        var values = $(this).serialize();
-        
-        updateStorageState({users: storageState.users.push(values)});
-        saveStorage();
-
-        // let data = JSON.stringify( $(form).serializeArray() );
-        // console.log(data);
-
-        // chrome.storage.local.set({key: value}, function() {
-        //     console.log('User saved');
-        //   });
-
+        let values = $(this).serializeArray().reduce((o,kv) => ({...o, [kv.name]: kv.value}), {});
+        addUser(values);
     });
+
+    // const addRepository = (repository) => {
+    //     repositories.add(repository);
+    //     chrome.storage.local.set(
+    //         {"repositories": JSON.stringify([...repositories])},
+    //         () => { 
+    //             chrome.storage.local.get("repositories", (storage) => {
+    //                 console.log(storage);
+    //             })
+    //          }
+    //     );
+    // }    
+
+
+    // const extendRepositories = (newUsers) => {
+    //     newUsers.forEach(repositories.add, repositories)
+    // }
+
+    // const loadLocalRepositories = () => {
+    //     chrome.storage.local.get("repositories", (storage) => {
+    //         loadedRepositories = JSON.parse(storage.repositories);
+    //         extendRepositories(loadedRepositories);
+    //     })
+    // }
+
+    
+
+    // $("#add-repo-form").submit(function(event) {
+    //     event.preventDefault();
+    //     let values = $(this).serializeArray().reduce((o,kv) => ({...o, [kv.name]: kv.value}), {});
+    //     addRepository(values);
+    // });
+
+    const loadLocalRepositories = () => {
+        chrome.storage.local.get("repositories", (storage) => {
+            loadedRepositories = JSON.parse(storage.repositories);
+            setUsers(loadedRepositories);
+        })
+    }
+
+    const addRepository = (repository) => {
+        loadLocalRepositories();
+        repositories.add(repository);
+        chrome.storage.local.set(
+            {"repository": JSON.stringify([...repositories])},
+            () => { 
+                chrome.storage.local.get("repository", (storage) => {
+                    console.log(storage);
+                }) 
+             }
+        );
+    }
+
+    $("#add-repo-form").submit(event => {
+        event.preventDefault();
+        let values = $(this).serializeArray().reduce((o,kv) => ({...o, [kv.name]: kv.value}), {});
+        addRepository(values);
+    });
+
+    loadLocalUsers();
+    loadLocalRepositories();
+
+    const buildUsersTable = () => {
+
+    }
 
 });
 
