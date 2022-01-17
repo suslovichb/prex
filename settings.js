73ae3310eafa33bc53e31b369d157937d7e69c98
Bohdan Suslovych <suslovichb@gmail.com>
2022-01-17 16:02:11 +0200

    Add users/repos tables building

--- settings.js
+++ settings.js
@@ -1,106 +1,129 @@
 $( document ).ready(function() {
 
+    const userFields = ["name", "link"];
+    const repositoryFields = ["link"];
+    const usersContainerId = "users-table-container"
+    const repositoriesContainerId = "repo-table-container"
+    const tableClasses = ["table", "table-hover"]
+
     let users = new Set();
     let repositories = new Set();
 
     const setUsers = (newUsers) => {
         users = new Set(newUsers);
+        buildTable(userFields, [...users], usersContainerId);
     }
 
     const setRepositories = (newRepositories) => {
         repositories = new Set(newRepositories);
+        buildTable(repositoryFields, [...repositories], repositoriesContainerId);
     }
 
-    const loadLocalUsers = () => {
-        chrome.storage.local.get("users", (storage) => {
-            loadedUsers = JSON.parse(storage.users);
-            setUsers(loadedUsers);
-        })
+    const readLocalStorage = async (key) => {
+        return new Promise((resolve, reject) => {
+            chrome.storage.local.get([key], function (storage) {
+                if (storage[key] === undefined) {
+                    reject();
+                } else {
+                    resolve(storage[key]);
+                }
+            });
+        });
+    };
+
+    const validateUsers = (users) => (Array.isArray(users) && users.length > 0);
+
+    const loadLocalUsers = async () => {
+        try {
+            let localUsers = await readLocalStorage("users");
+            let parsedUsers = JSON.parse(localUsers);
+            validateUsers(parsedUsers) && setUsers(parsedUsers);
+            return true;
+        } catch(err) {
+            console.log(err);
+            return false;
+        }  
     }
 
     const addUser = (user) => {
-        loadLocalUsers();
-        users.add(user);
-        chrome.storage.local.set(
-            {"users": JSON.stringify([...users])},
-            () => { 
-                chrome.storage.local.get("users", (storage) => {
-                    console.log(storage);
-                }) 
-             }
-        );
+        loadLocalUsers().then((isLoaded) => {
+            setUsers([...users, user]);
+            chrome.storage.local.set(
+                {"users": JSON.stringify([...users])},
+                () => {}
+            );
+        }).catch((err) => {console.log(err)});
     }
 
-    $("#add-user-form").submit(event => {
+    $("#add-user-form").submit(function(event) {
         event.preventDefault();
         let values = $(this).serializeArray().reduce((o,kv) => ({...o, [kv.name]: kv.value}), {});
         addUser(values);
     });
 
-    // const addRepository = (repository) => {
-    //     repositories.add(repository);
-    //     chrome.storage.local.set(
-    //         {"repositories": JSON.stringify([...repositories])},
-    //         () => { 
-    //             chrome.storage.local.get("repositories", (storage) => {
-    //                 console.log(storage);
-    //             })
-    //          }
-    //     );
-    // }    
+    const validateRepositories = (repositories) => (Array.isArray(repositories) && repositories.length > 0);
 
-
-    // const extendRepositories = (newUsers) => {
-    //     newUsers.forEach(repositories.add, repositories)
-    // }
-
-    // const loadLocalRepositories = () => {
-    //     chrome.storage.local.get("repositories", (storage) => {
-    //         loadedRepositories = JSON.parse(storage.repositories);
-    //         extendRepositories(loadedRepositories);
-    //     })
-    // }
-
-    
-
-    // $("#add-repo-form").submit(function(event) {
-    //     event.preventDefault();
-    //     let values = $(this).serializeArray().reduce((o,kv) => ({...o, [kv.name]: kv.value}), {});
-    //     addRepository(values);
-    // });
-
-    const loadLocalRepositories = () => {
-        chrome.storage.local.get("repositories", (storage) => {
-            loadedRepositories = JSON.parse(storage.repositories);
-            setUsers(loadedRepositories);
-        })
+    const loadLocalRepositories = async () => {
+        try {
+            let localRepositories = await readLocalStorage("repositories");
+            let parsedRepositories = JSON.parse(localRepositories);
+            validateRepositories(parsedRepositories) && setRepositories(parsedRepositories);
+            return true;
+        } catch(err) {
+            console.log(err);
+            return false;
+        }  
     }
 
     const addRepository = (repository) => {
-        loadLocalRepositories();
-        repositories.add(repository);
-        chrome.storage.local.set(
-            {"repository": JSON.stringify([...repositories])},
-            () => { 
-                chrome.storage.local.get("repository", (storage) => {
-                    console.log(storage);
-                }) 
-             }
-        );
+        loadLocalRepositories().then((isLoaded) => {
+            setRepositories([...repositories, repository]);
+            chrome.storage.local.set(
+                {"repositories": JSON.stringify([...repositories])},
+                () => {}
+            );
+        }).catch((err) => {console.log(err)});
     }
 
-    $("#add-repo-form").submit(event => {
+    $("#add-repo-form").submit(function(event) {
         event.preventDefault();
         let values = $(this).serializeArray().reduce((o,kv) => ({...o, [kv.name]: kv.value}), {});
         addRepository(values);
     });
 
+    const buildTable = (columnNames, items, htmlContainerId) => {
+        let table = makeTable(columnNames, items);
+        $("#" + htmlContainerId).html(table);
+    }
+    
+    const makeTable = (columnNames, items) => {
+        return (`
+            <table class="${tableClasses.join(" ")}">
+                <thead>
+                    ${makeHeaders(columnNames)}
+                </thead>
+                <tbody>
+                    ${makeRows(columnNames, items)}
+                </tbody>        
+            </table>
+        `);
+    }
+
+    const makeHeaders = (columnNames) => {
+        return columnNames.map((columnName) => `<th>${columnName}</th>`).join("");
+    }
+
+    const makeRows = (columnNames, items) => {
+        return items.map((item) => makeRow(columnNames, item)).join("")
+    }
+
+    const makeRow = (columnNames, item) => {
+        let cells = columnNames.map((columnName) => `<td>${item[columnName]}</td>`).join("");
+        return `<tr>${cells}</tr>`;
+    }
+
     loadLocalUsers();
     loadLocalRepositories();
-
-    const buildUsersTable = () => {
-
-    }
 
 });
 
