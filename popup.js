1b03cc32258911fc66b350b11dcb29f50176cbb7
Андрей Волотковский <evroandrew@gmail.com>
2022-01-25 21:09:56 +0900

    Report for closed prs (#24)
    * ui improvements
    
    * task 3 demo version
    
    * refactored
    
    Co-authored-by: evroandrew <volotkov@i.ua>

--- popup.js
+++ popup.js
@@ -1,11 +1,18 @@
 document.getElementById("settings-btn").addEventListener("click", () => {
     chrome.tabs.create({url: "settings.html"})
 });
+
 document.getElementById("report-btn").addEventListener("click", () => {
     chrome.storage.local.get(["teams"], (storage) => {
         const teams = JSON.parse(storage.teams);
         const selectedTeam = document.getElementById("team-select").value;
-        localStorage.setItem('Token', document.getElementById("token-input").value);
+        const reportType = getElementData(labelOpenedClosed).report;
+        localStorage.setItem('reportType', reportType);
+        if (reportType === 0) {
+            localStorage.setItem('startDate', JSON.stringify(document.getElementById('start-date').value));
+            localStorage.setItem('endDate', JSON.stringify(document.getElementById('end-date').value));
+        }
+        localStorage.setItem('token', document.getElementById("token-input").value);
         localStorage.setItem('selectedTeam', JSON.stringify(teams[selectedTeam]));
         chrome.tabs.create({url: "report.html"});
     });
@@ -20,6 +27,12 @@ document.getElementById("tokens-btn").addEventListener("click", () => {
 });
 
 $(document).ready(function () {
+    document.getElementById('start-date').value = today;
+    document.getElementById('end-date').value = today;
+    let inputs = document.getElementsByClassName('input-date');
+    for (const input in inputs) {
+        inputs[input].style = 'visibility: hidden; opacity: 0;';
+    }
     chrome.storage.local.get(["teams"], (storage) => {
         try {
             let teams = JSON.parse(storage.teams);
@@ -36,4 +49,33 @@ $(document).ready(function () {
             document.getElementById("report-btn").hidden = true;
         }
     });
+});
+
+const today = (new Date()).toISOString().split('T')[0];
+const setElementData = (el, data) => {
+    el.$dataKey = data;
+}
+const getElementData = (el) => {
+    return el.$dataKey;
+}
+setElementData(document.getElementById('labelOpenedClosed'), {
+    report: 1,
+});
+
+document.getElementById("report-switch").addEventListener("change", () => {
+    let labelOpenedClosed = document.getElementById('labelOpenedClosed');
+    let inputs = document.getElementsByClassName('input-date');
+    if (getElementData(labelOpenedClosed).report !== 1) {
+        setElementData(labelOpenedClosed, {report: 1});
+        labelOpenedClosed.innerText = 'Opened Pull Requests';
+        for (const input in inputs) {
+            inputs[input].style = 'visibility: hidden; opacity: 0;';
+        }
+    } else {
+        setElementData(labelOpenedClosed, {report: 0});
+        labelOpenedClosed.innerText = 'Closed Pull Requests';
+        for (const input in inputs) {
+            inputs[input].style = '';
+        }
+    }
 });
